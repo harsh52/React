7d94849279f333df469e64be04bf0a8aae7a4451
harsh52 <hkumar8897@gmail.com>
2021-01-10 13:07:19 +0530

    Simple hello there! Understand: basic of react

---
+++ demo Hello there!/src/index.js
@@ -0,0 +1,14 @@
+//import the React and reactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Create a react component
+const App = () => {
+    return <div> Hello </div>
+};
+
+//Take a eact component and show it on the screen
+ReactDOM.render(
+    <App />,
+    document.querySelector("#root")HTMLScriptElement
+);
