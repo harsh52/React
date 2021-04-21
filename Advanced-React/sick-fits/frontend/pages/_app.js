48e3036e4d732d550617c3bb5b4399dafdb3a6b0
harsh-sirpi <harsh@sirpi.io>
2021-04-21 15:03:44 +0530

    vid: 19: complete

--- Advanced-React/sick-fits/frontend/pages/_app.js
+++ Advanced-React/sick-fits/frontend/pages/_app.js
@@ -2,15 +2,31 @@ import Page from "../components/Page";
 import Router from 'next/router';
 import NProgress from 'nprogress';
 import '../components/styles/nprogress.css';
+import { ApolloProvider } from "@apollo/client";import { Component } from "react";
+import withData from "../lib/withData";
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
-export default function MyApp({Component, PageProps}){
 
+function MyApp({Component, pageProps,apollo}){
+    console.log(apollo)
     return(
-        <Page>
-            <Component {...PageProps} />
-        </Page>
-    )
+        <ApolloProvider client={apollo}>
+            <Page>
+                <Component {...pageProps} />
+            </Page>
+        </ApolloProvider>
+    );
 }
+
+MyApp.getInitialProps = async function({ component, ctx}){
+    let pageProps = {}
+    if(Component.getInitialProps){
+        pageProps = await Component.getInitialProps(ctx)  
+    }
+    pageProps.query = ctx.query;
+    return{pageProps};
+}
+
+export default withData(MyApp)
